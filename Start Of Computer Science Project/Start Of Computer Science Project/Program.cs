3a1bebf2964331b068fef743dd527f06870f8b5f
matsr22 <62033711+matsr22@users.noreply.github.com>
2021-01-04 14:19:05 +0000

    Add files via upload

---
+++ Start Of Computer Science Project/Start Of Computer Science Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Start_Of_Computer_Science_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
